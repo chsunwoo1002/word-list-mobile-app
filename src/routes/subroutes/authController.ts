9308fe1a110bd96d8016d43ba74a38be9496eb03
Sunwoo <chsunwoo1002@gmail.com>
2022-04-09 12:48:43 -0700

    add username/password validation for auth

--- src/routes/subroutes/authController.ts
+++ src/routes/subroutes/authController.ts
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import {loginModel} from '../../model/user';
+import * as EmailValidator from 'email-validator';
+import {validatePassword} from '../utils/authUtils';
 
 const authController: Express = express();
 const saltRound = 10;
@@ -11,20 +13,28 @@ mongoose.connect('mongodb://localhost:27017/login-app-db');
 authController.use(bodyParser.json());
 
 authController.post('/v1/register', (req, res) => {
-  const {username, pass} = req.body;
+  const {email, pass} = req.body;
 
-  // check username, password type -> fail return error message error: 'Invalid username'
-  // check password following rule
+  if (!email || typeof email !== 'string' || !EmailValidator.validate(email)) {
+    return res.json({status: 'error', error: 'Invalid username'});
+  }
+  if (!pass || typeof pass !== 'string' || validatePassword(pass)) {
+    return res.json({status: 'error', error: 'Invalid password'});
+  }
   (async () => {
     const password = await bcrypt.hash(pass, saltRound);
+    const username = email.toLowerCase();
     try {
-      const response = await loginModel.create({
+      await loginModel.create({
         username,
         password,
       });
-    } catch (error) {
-      //if (error.code === 11000) , error: 'Username already in use'
-      return res.json({status: 'error'});
+    } catch (error: unknown) {
+      if ((error as {code: number}).code === 11000) {
+        return res.json({status: 'error', error: 'Username already in use'});
+      } else {
+        return res.json({status: 'error', error: 'Unknown error in db'});
+      }
     }
     return res.json({status: 'ok'});
   })();
--- src/routes/utils/authUtils.ts
+++ src/routes/utils/authUtils.ts
@@ -1,3 +1,20 @@
+import PasswordValidator from 'password-validator';
 
+/**
+ * valid password
+ * contains at least 8 characters, uppercase, lowercase, digit
+ * not contains space
+ */
 
-export {};
+const validatePassword = (pass:string) => {
+  const schema = new PasswordValidator();
+  schema
+      .is().min(8)
+      .has().uppercase()
+      .has().lowercase()
+      .has().digit()
+      .has().not().space();
+  return schema.validate(pass);
+};
+
+export {validatePassword};
