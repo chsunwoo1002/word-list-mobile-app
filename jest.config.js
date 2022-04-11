cb361abd8cb1e581dcc742b9dc007c03eeb4271f
Sunwoo <chsunwoo1002@gmail.com>
2022-04-10 21:19:06 -0700

    set configuration jest with typescript

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
+// jest.config.js
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
